de4a23aac37a13af01dd8d8d6037f71cf38cf9e5
Максим Аммосов <_@example.com>
2025-05-06 15:08:12 +0900

    Сохраняю изменения в App.js

--- ammosovMaxim/App.js
+++ ammosovMaxim/App.js
@@ -1,19 +1,44 @@
 import React, { useEffect } from 'react';
-import { Provider } from 'react-redux';
+import { SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
+import { Provider, useSelector } from 'react-redux';
 import { store } from './store';
 import { loadTheme } from './store/themeSlice';
 import Lab4 from './screens/Lab4';
 
-const App = () => {
+const AppContent = () => {
+  const isDarkMode = useSelector(state => state.theme.isDarkMode);
+
+  const containerStyle = {
+    flex: 1,
+    backgroundColor: isDarkMode ? '#1a1a1a' : '#fff',
+  };
+
   useEffect(() => {
     store.dispatch(loadTheme());
   }, []);
 
   return (
+    <SafeAreaView style={containerStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={styles.container}>
+        <Lab4 />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const App = () => {
+  return (
     <Provider store={store}>
-      <Lab4 />
+      <AppContent />
     </Provider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App; 
