165182225b0bfd009840230121f1bb7f258b6171
Максим Аммосов <_@example.com>
2025-05-08 23:30:32 +0900

    обновил app

--- ammosovMaxim/App.js
+++ ammosovMaxim/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Lab1 from './screens/Lab1';
+import Lab2 from './screens/Lab2';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Lab1">
         <Stack.Screen name="Lab1" component={Lab1} options={{ title: 'Лабораторная 1' }} />
+        <Stack.Screen name="Lab2" component={Lab2} options={{ title: 'Лабораторная 2' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
